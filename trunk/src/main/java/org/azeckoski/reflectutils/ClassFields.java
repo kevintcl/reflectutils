e9b653d49efd7c8ec7aa746beab3c078b3f3d139
azeckoski <azeckoski@1aec8994-8edb-11dd-94f4-d555c0a34076>
2008-10-02 16:01:07 +0000

    Fixed unused import

--- trunk/src/main/java/org/azeckoski/reflectutils/ClassFields.java
+++ trunk/src/main/java/org/azeckoski/reflectutils/ClassFields.java
@@ -21,7 +21,6 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
